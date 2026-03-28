b9fe95eccc630d112a8e495250eb82f436bde69d
Takuto Hachiya <hachiya.works31@gmail.com>
2026-03-28 21:08:39 +0900

    initial commit

---
+++ app/Http/Controllers/HomebudgetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\HomeBudget;
+
+class HomebudgetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('homebudget.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'category' => 'required|numeric',
+            'price' => 'required|numeric',
+        ]);
+
+        $result = HomeBudget::create([
+            'date' => $request->date,
+            'category_id' => $request->category,
+            'price' => $request->price,
+        ]);
+
+        if (!empty($result)) {
+            session()->flash('flash_message', '支出を登録しました');
+        } else {
+            session()->flash('flash_error_message', '支出の登録に失敗しました');
+        }
+
+        return redirect()->route('index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php   
+use App\Http\Controllers\HomebudgetController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('homebudget.index');
+});
+
+Route::get('/', [HomebudgetController::class, 'index'])->name('index');
+Route::post('/post', [HomebudgetController::class, 'store'])->name('store');
+Route::get('/edit/{id}', [HomebudgetController::class, 'edit'])->name('homebudget.edit');
+Route::put('/update/{id}', [HomebudgetController::class, 'update'])->name('homebudget.update');
+Route::post('/destroy/{id}', [HomebudgetController::class, 'destroy'])->name('homebudget.destroy');
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
